586cf87a4f9e2dc93090ea049bf883070258bbc1
Adam Hamot <Rjhoops92@gmail.com>
2012-08-27 17:29:24 -0400

    Added mousetrap into index

--- public/javascripts/y86.js
+++ public/javascripts/y86.js
@@ -1,8 +1,6 @@
 $("#commandBox").val("=> ");
 
-$("#commandBox").keypress(function(e) {
-  if (e.which == 13) {
-    event.preventDefault();
-    $("#commandBox").val(function(i, v) {  return v + "\n=> "; });
-  }
+Mousetrap.bind('enter', function() {
+  event.preventDefault();
+  $("#commandBox").val(function(i, v) {  return v + "\n=> "; });
 });
