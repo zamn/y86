7470a3d6c82c4637c607f8b70ab53b431fe1acf9
Zach Fogg <zach@zachfogg.com>
2012-08-27 18:48:17 -0400

    Fixed textarea scrolling.

--- public/javascripts/y86.js
+++ public/javascripts/y86.js
@@ -1,7 +1,17 @@
-$("#commandBox").val("=> ");
-
-Mousetrap.bind('enter', function(e) {
-  e.preventDefault();
-  $("#commandBox").val(function(i, v) {  return v + "\n=> "; });
-  return false;
+$("#commandBox").focus(function() {
+  Mousetrap.bind("enter", function(e) {
+    $("#commandBox").val(function(i, v) {  return v + "\n=> "; });
+    $("#commandBox").scrollTop(99999);
+    return false;
+  });
 });
+$("#commandBox").blur(function() {
+  Mousetrap.unbind("enter");
+});
+
+$("#commandBox").bind("keyup change", function() {
+  this.value = this.value;
+});
+
+$("#commandBox").focus();
+$("#commandBox").val("=> ");
