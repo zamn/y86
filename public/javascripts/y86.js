f7629699b2da0f24c8422000585dc7512fc323d3
Adam Hamot <Rjhoops92@gmail.com>
2012-08-27 22:37:40 -0400

    Handles error codes from interpretation and also unescapes JSON.

--- public/javascripts/y86.js
+++ public/javascripts/y86.js
@@ -2,16 +2,43 @@ $("#commandBox").focus(function() {
   Mousetrap.bind("enter", function(e) {
     var commands = $("#commandBox").val().split("\n");
     var command = commands[commands.length-1].substring(3);
-    console.log(command);
     if (command === "clear") {
       $("#commandBox").val("=> ");
     }
     else {
-      $.post("/y86/interpret?command=" + command);
-      $("#commandBox").val(function(i, v) {  return v + "\n=> "; });
-      $("#commandBox").scrollTop(99999);
+      var returnCode;
+      $.post("/y86/interpret?command=" + command, function(data) {
+        returnCode = parseInt(data, 10);
+        console.log(returnCode);
+        console.log(typeof(returnCode));
+        switch (returnCode) {
+          case 0:
+            $("#commandBox").val(function(i, v) {  return v + "\nStatement Completed."; });
+            break;
+          case 1:
+            console.log('hi');
+            $("#commandBox").val(function(i, v) {  return v + "\nMemory out of bounds!"; });
+            break;
+          case 2:
+            $("#commandBox").val(function(i, v) {  return v + "\nNon-existant label for JMP operation"; });
+            break;
+          case 3:
+            $("#commandBox").val(function(i, v) {  return v + "\nPlease input integer/character"; });
+            break;
+          case 4:
+            $("#commandBox").val(function(i, v) {  return v + "\nEmpty Stack."; });
+            break;
+          case 5:
+            $.post("/y86/state?type=output", function(data) {
+              $("#commandBox").val(function(i, v) {  return v + "\nOutput: " + data; });
+            });
+            break;
+        }
+        $("#commandBox").val(function(i, v) {  return v + "\n=> "; });
+        $("#commandBox").scrollTop(99999);
+      });
     }
-    return false;
+  return false;
   });
 });
 
