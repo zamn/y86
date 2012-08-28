e51dbd7e821443462badf055b81cfd68b289d90c
Zach Fogg <zach@zachfogg.com>
2012-08-27 23:55:17 -0400

    Update the #registersArea with test data. Some HTML and CSS changes.

--- public/javascripts/y86.js
+++ public/javascripts/y86.js
@@ -1,3 +1,7 @@
+var updateCommandBox = function(text) {
+  $("#commandBox").val(function(i, v) {  return v + text; });
+}
+
 $("#commandBox").focus(function() {
   Mousetrap.bind("enter", function(e) {
     var commands = $("#commandBox").val().split("\n");
@@ -5,43 +9,67 @@ $("#commandBox").focus(function() {
     if (command === "clear") {
       $("#commandBox").val("=> ");
     }
-    else {
-      var returnCode;
-      $.post("/y86/interpret?command=" + command, function(data) {
-        returnCode = parseInt(data, 10);
-        console.log(returnCode);
-        console.log(typeof(returnCode));
-        switch (returnCode) {
+    else if (command !== "") {
+      $.post("/y86/interpret?command=" + command, function(returnCode) {
+        switch (parseInt(returnCode, 10)) {
           case 0:
-            $("#commandBox").val(function(i, v) {  return v + "\nStatement Completed."; });
+            updateCommandBox("\nStatement Completed.");
             break;
           case 1:
-            console.log('hi');
-            $("#commandBox").val(function(i, v) {  return v + "\nMemory out of bounds!"; });
+            updateCommandBox("\nMemory out of bounds!");
             break;
           case 2:
-            $("#commandBox").val(function(i, v) {  return v + "\nNon-existant label for JMP operation"; });
+            updateCommandBox("\nNon-existant label for JMP operation");
             break;
           case 3:
-            $("#commandBox").val(function(i, v) {  return v + "\nPlease input integer/character"; });
+            updateCommandBox("\nPlease input integer/character");
             break;
           case 4:
-            $("#commandBox").val(function(i, v) {  return v + "\nEmpty Stack."; });
+            updateCommandBox("\nEmpty Stack.");
             break;
           case 5:
             $.post("/y86/state?type=output", function(data) {
-              $("#commandBox").val(function(i, v) {  return v + "\nOutput: " + data; });
+              updateCommandBox("\nOutput: " + data);
             });
             break;
         }
+
         $("#commandBox").val(function(i, v) {  return v + "\n=> "; });
         $("#commandBox").scrollTop(99999);
       });
     }
 
   $.getJSON("/y86/state", function(data) {
-      console.log("Great success!");
-      console.log(data);
+    var registers      = data[0],
+        flags          = data[1],
+        stack          = data[2],
+        programCounter = data[3][0];
+
+    //Parse to proper types where necessary.
+    registers      = registers.map(function(x) { return parseInt(x, 10); });
+    flags          = flags.map(    function(x) { return x === "true";    });
+
+    //Update the registers table.
+    $("#registers tr > :nth-child(2)").each(function(i, x) {
+      $(x).text(registers[i]);
+    });
+
+    //Update the flags table.
+    $("#pointers tr > :nth-child(2)").each(function(i, x) {
+      $(x).text(flags[i]);
+    });
+
+    //Update the programCounter table.
+    $("#programCounter td").text(programCounter);
+
+    //Clear and update the stack table.
+    $("#stack tr td").parent().remove();
+    stack.forEach(function(x) {
+      $("#stack").append(
+        "<tr><td>"+x+"</td></tr>"
+        );
+    });
+
   });
 
   return false;
