ae2878d16de9b2888538be4885d441f195b733d8
Adam Hamot <Rjhoops92@gmail.com>
2012-08-27 21:30:08 -0400

    Working on example java file

---
+++ app/models/Test.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.*;
+
+public class Test {
+
+  public int interpret(String message) {
+    Random r = new Random();
+    int derp = r.nextInt(10);
+    return derp;
+  }
+
+}
